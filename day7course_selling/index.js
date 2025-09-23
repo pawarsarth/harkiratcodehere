707f157b3fe1dbc9bf7df539861a74fa2a866bed
Sarthak Pawar <pawar.sarthak24@gmail.com>
2025-09-23 15:26:35 +0530

    day7

--- day7course_selling/index.js
+++ day7course_selling/index.js
@@ -2,7 +2,38 @@ const express=require('express')
 
 const app=express();
 
+app.use(express.json())
 
+app.post('/user/signup',function(req,res){
+    res.json({
+
+    })
+})
+
+app.post('/user/signin',function(req,res){
+
+    res.json({
+        message:"post user signin"
+    })
+})
+app.get('/user/purchases',function(req,res){
+    res.json({
+        message:"purchases"
+    })
+
+})
+app.post('/course/purchase',function(req,res){
+
+    res.json({
+        message:"purchses here"
+    })
+})
+
+app.get('/courses',function(req,res){
+        res.json({
+            message:"courses"
+        })
+})
 
 
 app.listen(3000)
