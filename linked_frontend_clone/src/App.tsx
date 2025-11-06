11bbf81f448bff8ebde145f4574a0bed26b9032f
Sarthak Pawar <pawar.sarthak24@gmail.com>
2025-11-06 23:20:48 +0530

    fixed build setup for Render deployment

--- linked_frontend_clone/src/App.tsx
+++ linked_frontend_clone/src/App.tsx
@@ -1,21 +1,23 @@
-
-
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Sigin'
 import { Dashboard } from './pages/Dashboard'
 
 function App() {
-
-
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path='/signup' element={<Signup></Signup>}></Route>
-           <Route path='/signin' element={<Signin></Signin>}></Route>
-           <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
-        </Routes>
+      <Routes>
+        {/* ✅ Default route (redirect to signin) */}
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* ✅ Fallback route for undefined paths */}
+        <Route path="*" element={<Navigate to="/signin" replace />} />
+      </Routes>
     </BrowserRouter>
   )
 }
