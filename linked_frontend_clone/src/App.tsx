08426d3d933377f4360fc5dc6a4616de07a3c222
Sarthak Pawar <pawar.sarthak24@gmail.com>
2025-11-06 09:00:22 +0530

    done234

---
+++ linked_frontend_clone/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='bg-black text-white'>
+      welcome sarthak here 
+
+    </div>
+  )
+}
+
+export default App
